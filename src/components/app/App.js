4857b7062ddd7a51cb03dab8865d3bdb2b716124
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-17 23:27:36 +0300

    Testing out grids

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,14 @@ function App() {
   return (
     <div>
       <p>TEST</p>
+      <div className="container">
+        <div className="item item--1">1: Orange</div>
+        <div className="item item--2">2: Olive</div>
+        <div className="item item--3">3: Salmon</div>
+        <div className="item item--4">4: Sky Blue</div>
+        <div className="item item--5">5: Hot Pink</div>
+        <div className="item item--6">6: Medium Purple</div>
+      </div>
     </div>
   );
 }
