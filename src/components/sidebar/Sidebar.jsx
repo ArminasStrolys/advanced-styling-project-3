b557adf275f15f70f6e0ddc6d95a27ec07d50d4a
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-19 23:59:00 +0300

    Grid layout in progress

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Sidebar = () => {
-  return <div className="sidebar">SIDEBAR</div>;
+  return (
+    <div className="sidebar">
+      <p>SIDEBAR</p>
+    </div>
+  );
 };
 
 export default Sidebar;
