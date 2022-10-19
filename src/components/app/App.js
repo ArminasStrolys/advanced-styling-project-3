8549babcea21639f1decf4ee047ee24ef614fdfa
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-19 13:55:27 +0300

    Fresh start to take on grid practical project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,37 +1,5 @@
 function App() {
-  return (
-    <div>
-      <p>TEST</p>
-      {/* <div className="container">
-        <div className="item item--1">1: Orange</div>
-        <div className="item item--2">2: Olive</div>
-        <div className="item item--3">3: Salmon</div>
-        <div className="item item--4">4: Sky Blue</div>
-        <div className="item item--5">5: Hot Pink</div>
-        <div className="item item--6">6: Medium Purple</div>
-      </div> */}
-
-      {/* <div className="grid-container">
-        <div className="grid-item item-1">Header</div>
-        <div className="grid-item item-2">Sidebar</div>
-        <div className="grid-item item-3">Small box 1</div>
-        <div className="grid-item item-4">Small box 2</div>
-        <div className="grid-item item-5">Small box 3</div>
-        <div className="grid-item item-6">Main content</div>
-        <div className="grid-item item-7">Footer</div>
-      </div> */}
-      <div className="grid-container">
-        <div className="grid-item item-1">Modern</div>
-        <div className="grid-item item-2">CSS</div>
-        <div className="grid-item item-3">With</div>
-        <div className="grid-item item-4">Flexbox</div>
-        <div className="grid-item item-5">And</div>
-        <div className="grid-item item-6">Grid</div>
-        <div className="grid-item item-7">Is</div>
-        <div className="grid-item item-8">Great</div>
-      </div>
-    </div>
-  );
+  return <div>TESTING</div>;
 }
 
 export default App;
