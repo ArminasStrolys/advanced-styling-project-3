ff572312bfe7959c7d5783add7a9af6dd79d1b74
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-20 22:16:23 +0300

    Feture section start

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Features = () => {
-  return <div className="features">FEATURES</div>;
+  return (
+    <div className="features">
+      <div className="feature">feature 1</div>
+      <div className="feature">feature 2</div>
+      <div className="feature">feature 3</div>
+      <div className="feature">feature 4</div>
+      <div className="feature">feature 5</div>
+      <div className="feature">feature 6</div>
+    </div>
+  );
 };
 
 export default Features;
