b271214a59698af4d9d4ae6cc4276df3d3e75c87
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-20 22:45:01 +0300

    Filling feature blocks with fillers

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
+import global from '../../images/globe.svg';
 
 const Features = () => {
   return (
     <div className="features">
-      <div className="feature">feature 1</div>
-      <div className="feature">feature 2</div>
-      <div className="feature">feature 3</div>
-      <div className="feature">feature 4</div>
-      <div className="feature">feature 5</div>
-      <div className="feature">feature 6</div>
+      <div className="feature">
+        <img feature__icon src={global} alt="" />
+        <h4 className="heading-4">Best choice for your luxury home.</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          unde maiores explicabo minima incidunt saepe tempore in est porro, hic
+          deleniti iusto possimus totam aut deserunt necessitatibus. Fugiat,
+          excepturi deserunt.
+        </p>
+      </div>
+      <div className="feature">
+        <img feature__icon src={global} alt="" />
+        <h4 className="heading-4">Best choice for your luxury home.</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          unde maiores explicabo minima incidunt saepe tempore in est porro, hic
+          deleniti iusto possimus totam aut deserunt necessitatibus. Fugiat,
+          excepturi deserunt.
+        </p>
+      </div>
+      <div className="feature">
+        <img feature__icon src={global} alt="" />
+        <h4 className="heading-4">Best choice for your luxury home.</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          unde maiores explicabo minima incidunt saepe tempore in est porro, hic
+          deleniti iusto possimus totam aut deserunt necessitatibus. Fugiat,
+          excepturi deserunt.
+        </p>
+      </div>
+      <div className="feature">
+        <img feature__icon src={global} alt="" />
+        <h4 className="heading-4">Best choice for your luxury home.</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          unde maiores explicabo minima incidunt saepe tempore in est porro, hic
+          deleniti iusto possimus totam aut deserunt necessitatibus. Fugiat,
+          excepturi deserunt.
+        </p>
+      </div>
+      <div className="feature">
+        <img feature__icon src={global} alt="" />
+        <h4 className="heading-4">Best choice for your luxury home.</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          unde maiores explicabo minima incidunt saepe tempore in est porro, hic
+          deleniti iusto possimus totam aut deserunt necessitatibus. Fugiat,
+          excepturi deserunt.
+        </p>
+      </div>
+      <div className="feature">
+        <img feature__icon src={global} alt="" />
+        <h4 className="heading-4">Best choice for your luxury home.</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          unde maiores explicabo minima incidunt saepe tempore in est porro, hic
+          deleniti iusto possimus totam aut deserunt necessitatibus. Fugiat,
+          excepturi deserunt.
+        </p>
+      </div>
     </div>
   );
 };
