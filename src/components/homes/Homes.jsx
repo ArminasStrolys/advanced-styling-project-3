a151ece5ff3daf7b412e658fa18ee06ab07ebdab
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-21 18:21:46 +0300

    Card component creation process

--- src/components/homes/Homes.jsx
+++ src/components/homes/Homes.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import house1 from '../../images/house-1.jpeg';
 
 const Homes = () => {
-  return <div className="homes">HOMES</div>;
+  return (
+    <div className="homes">
+      <div className="home">
+        <img src={house1} alt="House 1" className="home__image" />
+        <div className="home__like">&nbsp;</div>
+        <h5 className="home__name">Beautiful family house</h5>
+        <div className="home__location">
+          <div className="home__pin">&nbsp;</div>
+        </div>
+        <p>PARIS</p>
+      </div>
+      <div className="home">Home card 2</div>
+      <div className="home">Home card 3</div>
+      <div className="home">Home card 4</div>
+      <div className="home">Home card 5</div>
+      <div className="home">Home card 6</div>
+    </div>
+  );
 };
 
 export default Homes;
