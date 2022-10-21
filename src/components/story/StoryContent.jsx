a1a375817c253eec56b515a563b1aabfff2cefaf
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-21 15:38:13 +0300

    Story section in progress

--- src/components/story/StoryContent.jsx
+++ src/components/story/StoryContent.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const StoryContent = () => {
-  return <div className="story__content">CONTENT</div>;
+  return (
+    <div className="story__content">
+      <h3 className="heading-3 mb-sm">Satisfied customers</h3>
+      <h2 className="heading-2 heading-2--dark mb-md">
+        &ldquo;Glad to chose realtors by this project&rdquo;
+      </h2>
+      <p className="story__text mb-md">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ratione
+        dignissimos magnam, blanditiis natus iure vero. Cum nam illo unde,
+        soluta repudiandae explicabo quas cupiditate, quibusdam mollitia
+        excepturi officiis vero quaerat qui. Sed doloremque velit asperiores
+        accusamus impedit! Maxime reiciendis modi reprehenderit quidem quam
+        assumenda autem facilis quia.
+      </p>
+      <button className="btn">Try our services</button>
+    </div>
+  );
 };
 
 export default StoryContent;
