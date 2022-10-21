b8cf7aa55efc4730289fffca2ad1c692ac226019
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-21 16:14:13 +0300

    Building the story - photos section. Manipulating images in the grid

--- src/components/story/StoryPictures.jsx
+++ src/components/story/StoryPictures.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import pic1 from '../../images/story-1.jpeg';
+import pic2 from '../../images/story-2.jpeg';
 
 const StoryPictures = () => {
-  return <div className="story__pictures">PICTURES</div>;
+  return (
+    <div className="story__pictures">
+      PICTURES
+      <img className="story__img--1" src={pic1} alt="" />
+      <img className="story__img--2" src={pic2} alt="" />
+    </div>
+  );
 };
 
 export default StoryPictures;
