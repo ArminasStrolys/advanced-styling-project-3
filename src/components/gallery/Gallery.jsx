1b1be5d412bbf3a8537ffa25f067e326512e0b9d
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-22 20:01:26 +0300

    Gallery section - grid layout

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import img1 from '../../images/gal-1.jpeg';
+import img2 from '../../images/gal-2.jpeg';
+import img3 from '../../images/gal-3.jpeg';
+import img4 from '../../images/gal-4.jpeg';
+import img5 from '../../images/gal-5.jpeg';
+import img6 from '../../images/gal-6.jpeg';
+import img7 from '../../images/gal-7.jpeg';
+import img8 from '../../images/gal-8.jpeg';
+import img9 from '../../images/gal-9.jpeg';
+import img10 from '../../images/gal-10.jpeg';
+import img11 from '../../images/gal-11.jpeg';
+import img12 from '../../images/gal-12.jpeg';
+import img13 from '../../images/gal-13.jpeg';
+import img14 from '../../images/gal-14.jpeg';
 
 const Gallery = () => {
-  return <div className="gallery">GALLERY</div>;
+  return (
+    <section className="gallery">
+      <img src={img1} alt="Gallery pic 1" className="gallery__img" />
+      <img src={img2} alt="Gallery pic 2" className="gallery__img" />
+    </section>
+  );
 };
 
 export default Gallery;
