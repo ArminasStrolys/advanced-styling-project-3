5ba92bd90ffba681fc82207b0e43dfe0c1a9acb3
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-23 15:16:50 +0300

    Footer content layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div className="footer">FOOTER</div>;
+  return (
+    <div className="footer">
+      <ul className="nav">
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Find your dream house
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Request proposal
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Download home planner
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Contact us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Submit your property
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            Come work with us
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
