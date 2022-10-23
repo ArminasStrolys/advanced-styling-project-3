a368adeb2df963ca3d870f8ff9bc44cdf0f7010b
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-23 16:02:09 +0300

    Sidebar section - added a nav button with after/before

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <p>SIDEBAR</p>
+      <button className="nav-btn"></button>
     </div>
   );
 };
